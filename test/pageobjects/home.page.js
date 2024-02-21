35f34a08b5440d413346a9d73f4a0bab880a10de
Maidzolaola <maidzola@hijra.id>
2024-02-21 14:03:38 +0700

    commit ke-4

--- test/pageobjects/home.page.js
+++ test/pageobjects/home.page.js
@@ -4,6 +4,8 @@ const Page = require('./page');
 class HomePage extends Page {
     get iconCart () { return $('.shopping_cart_link');}
     get buttonBackPack () { return $('#add-to-cart-sauce-labs-backpack');}
+    get buttonCart () { return $('#shopping_cart_container');}
+    get buttonCheckout () { return $('#checkout');}
 
 
     async validateHomePage() {
@@ -13,6 +15,14 @@ class HomePage extends Page {
 
     async validateAddCart() {
         await this.buttonBackPack.click()
+    }
+
+    async validateCart() {
+        await this.buttonCart.click()
+    }
+
+    async validateCheckout() {
+        await this.buttonCheckout.click()
     }
     open () {
         return super.open('/inventory.html');
--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -3,6 +3,8 @@ const HomePage = require('../pageobjects/home.page')
 
 
 describe('Swag Labs', () => {
+
+    //Scenario Login Page
     it('should login success with standard_user credentials', async () => {
         await LoginPage.open()
         await LoginPage.login(process.env.USERNAME_STANDARD_USER, process.env.PASSWORD_SAUCEDEMO)
@@ -18,12 +20,12 @@ describe('Swag Labs', () => {
         await LoginPage.login(process.env.USERNAME_PROBLEM_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validateHomePage()
     })
-    it('Should Login error with performance_glitch_user', async() =>{
-        await LoginPage.open()
-        await LoginPage.login(process.env.USERNAME_PERFORMANCE_GLITCH_USER, process.env.PASSWORD_SAUCEDEMO)
-        await HomePage.validateHomePage()
-        await LoginPage.validateGlitchUser()
-    })
+    // it('Should Login error with performance_glitch_user', async() =>{
+    //     await LoginPage.open()
+    //     await LoginPage.login(process.env.USERNAME_PERFORMANCE_GLITCH_USER, process.env.PASSWORD_SAUCEDEMO)
+    //     await HomePage.validateHomePage()
+    //     await LoginPage.validateGlitchUser()
+    // })
     it('Should Login error with error_user', async() =>{
         await LoginPage.open()
         await LoginPage.login(process.env.USERNAME_ERROR_USER, process.env.PASSWORD_SAUCEDEMO)
@@ -34,10 +36,25 @@ describe('Swag Labs', () => {
         await LoginPage.login(process.env.USERNAME_VISUAL_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validateHomePage()
     })
+
+    //Scenario Homepage
     it('Should Add to Cart', async() =>{
         await LoginPage.open()
-        await LoginPage.login(process.env.USERNAME_VISUAL_USER, process.env.PASSWORD_SAUCEDEMO)
+        await LoginPage.login(process.env.USERNAME_STANDARD_USER, process.env.PASSWORD_SAUCEDEMO)
         await HomePage.validateHomePage()
         await HomePage.validateAddCart()
+    })
+    it('Click Shopping Cart', async() =>{
+        await LoginPage.open()
+        await LoginPage.login(process.env.USERNAME_STANDARD_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validateHomePage()
+        await HomePage.validateCart()
+    })
+    it('Click Checkout Button', async() =>{
+        await LoginPage.open()
+        await LoginPage.login(process.env.USERNAME_STANDARD_USER, process.env.PASSWORD_SAUCEDEMO)
+        await HomePage.validateHomePage()
+        await HomePage.validateCart()
+        await HomePage.validateCheckout()
     })
 })
